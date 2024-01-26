b843e3309fc2dd8c0b92803781cc73f4725ac6e9
2188349576 <2188349576@qq.com>
2024-01-26 22:05:24 +0800

    Initial commit

---
+++ src/main/java/cn/hairuosky/xiequipmentreform/XiEquipmentReform.java
@@ -0,0 +1,17 @@
+package cn.hairuosky.xiequipmentreform;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XiEquipmentReform extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
